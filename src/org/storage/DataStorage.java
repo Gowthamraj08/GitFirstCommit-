f1b7b7b1d78d4933af698f804e8ce5ae8a377c70
Gowthamraj08 <gowthamraj0812@gmail.com>
2023-06-14 14:40:00 +0530

    Intial Commit

---
+++ src/org/storage/DataStorage.java
@@ -0,0 +1,14 @@
+package org.storage;
+
+public class DataStorage {
+	public void name() {
+		System.out.println("Gowtham Raj");
+
+	}
+
+	public static void main(String[] args) {
+		DataStorage a = new DataStorage();
+		a.name();
+	}
+
+}
